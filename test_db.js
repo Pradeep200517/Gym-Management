ebb294579f136ef5f6588557848215ffce4d5bce
Pradeep200517 <gundupradeep2005@gmail.com>
2025-05-25 13:16:47 +0530

    Update test_db.js

--- test_db.js
+++ test_db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Pradeep@2005', // Your MySQL password
+  password: '', // Your MySQL password
   database: 'gym_management'
 });
 
