39cd90087518d6ef48b5586aefa61fe822c434f5
Pradeep200517 <gundupradeep2005@gmail.com>
2025-05-25 13:16:29 +0530

    Update setup_database.bat

--- setup_database.bat
+++ setup_database.bat
@@ -3,7 +3,7 @@ echo Setting up the Gym Management System database...
 
 REM Replace with your MySQL username and password
 set MYSQL_USER=root
-set MYSQL_PASSWORD=Pradeep@2005
+set MYSQL_PASSWORD=
 
 REM Run the SQL script
 mysql -u %MYSQL_USER% -p%MYSQL_PASSWORD% < database.sql
